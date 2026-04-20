4cfa32a986e3506181409f8807daaa4c529b66e3
Bartosz <bartek.papierniak@gmail.com>
2026-04-20 19:45:04 +0200

    feat: add product_detail view with URL parameter

--- inventory/views.py
+++ inventory/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def product_list(request):
     return HttpResponse("Lista produktów - w budowie")
+
+def product_detail(request, product_id):
+    return HttpResponse(f"Szczegóły produktu nr: {product_id}")
