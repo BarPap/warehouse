dd345fef2e9008a40bd107e30ca9dc8256270c55
Bartosz <bartek.papierniak@gmail.com>
2026-04-20 21:11:29 +0200

    feat: add product_list template with context

--- inventory/views.py
+++ inventory/views.py
@@ -3,7 +3,13 @@ from django.http import HttpResponse
 
 
 def product_list(request):
-    return HttpResponse("Lista produktów - w budowie")
+    products = [
+        {'name': 'Śrubka M8', 'quantity': 100},
+        {'name': 'Nakrętka M8', 'quantity': 50},
+        {'name': 'Podkładka stalowa', 'quantity': 200},
+    ]   
+    context = {"products": products}
+    return render(request, "inventory/product_list.html", context)
 
 def product_detail(request, product_id):
     return HttpResponse(f"Szczegóły produktu nr: {product_id}")
